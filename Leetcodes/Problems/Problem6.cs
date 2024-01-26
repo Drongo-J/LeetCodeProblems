e4af87fbb6d650aaa11875619fb84969f2fe2c46
Aykhan Ahmadzada <ayxanx17@gmail.com>
2024-01-26 13:43:02 +0400

    Updated Problem 6

--- Leetcodes/Problems/Problem6.cs
+++ Leetcodes/Problems/Problem6.cs
@@ -17,7 +17,6 @@ namespace Leetcodes.Problems
             for (int i = 0; i < numRows; i++) 
             {
                 var index = i;
-                var inBetweenIndex = -1;
 
                 while (index < s.Length)
                 {
@@ -27,7 +26,7 @@ namespace Leetcodes.Problems
 
                     if (i != 0 && i != numRows - 1 && index > numRows)
                     {
-                        inBetweenIndex = index - (i * 2);
+                        var inBetweenIndex = index - (i * 2);
                         if (inBetweenIndex < s.Length)
                         {
                             newString += s[inBetweenIndex];
@@ -39,3 +38,36 @@ namespace Leetcodes.Problems
         }
     }   
 }
+
+//  P A Y P A L I S H I R  I  N  G
+//  0 1 2 3 4 5 6 7 8 9 10 11 12 13
+
+//  numRows = 4
+
+//  i = 0: 0 6 12
+//  i = 1: 1 5 7 11 13
+//  i = 2: 2 4 8 10
+//  i = 3: 3 9
+
+//  i = 0: P I    N
+//  i = 1: A L S  I G
+//  i = 2: Y A   H R
+//  i = 3: P I
+
+
+//         0     6       12
+//         1   5 7    11 13
+//         2 4   8 10    
+//         3     9       
+	    	
+//  0 6 12 1 5 7 11 13 2 4 8 10 3 9 
+//  PAYPALISHIRING
+//  PINALSIGYAHRPI
+
+
+//    i = 0   0         10             20
+//    i = 1   1       9 11          19 21
+//    i = 2   2     8   12       18    22
+//    i = 3   3   7     13    17       23
+//    i = 4   4 6       14 16          24
+//    i = 5   5         15             25
